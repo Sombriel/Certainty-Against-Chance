1adcf81e5f6c74f0e086c5f2d14c4eb7db9957e2
stacks-Gabe <ggarci78@lion.lmu.edu>
2026-06-21 00:09:41 -0700

    made dice spawn in a range between 1 and 3

--- code/scripts/boss/attacks/dice_attack.gd
+++ code/scripts/boss/attacks/dice_attack.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var dice: PackedScene
 @onready var paths: Array = [$PathONE/PathFollow2D, $PathTWO/PathFollow2D, $PathTHREE/PathFollow2D]
 
-@export var dice_count: int = 2  # how many dice to spawn per attack
+@export var dice_count: int = randi_range(1, 4) 
 
 func start() -> void:
 	var available_paths: Array = paths.duplicate()
@@ -11,8 +11,8 @@ func start() -> void:
 
 	for i in range(min(dice_count, available_paths.size())):
 		var chosen_path: PathFollow2D = available_paths[i]
-		chosen_path.progress_ratio = 0.0  # reset in case this path was used before
-		chosen_path.set_process(true)     # re-enable in case it was turned off last time
+		chosen_path.progress_ratio = 0.0 
+		#chosen_path.set_process(true)
 
 		var dice_scene: Area2D = dice.instantiate()
 		chosen_path.add_child(dice_scene)
