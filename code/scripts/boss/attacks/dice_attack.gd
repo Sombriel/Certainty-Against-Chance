e8dde4267fc1e21b6399175fc24f668bb4a65ec0
stacks-Gabe <ggarci78@lion.lmu.edu>
2026-06-21 00:08:17 -0700

    dice pathing works and multiple dice have a chance to spawn

--- code/scripts/boss/attacks/dice_attack.gd
+++ code/scripts/boss/attacks/dice_attack.gd
@@ -3,9 +3,16 @@ extends Node2D
 @export var dice: PackedScene
 @onready var paths: Array = [$PathONE/PathFollow2D, $PathTWO/PathFollow2D, $PathTHREE/PathFollow2D]
 
-func start() -> void:
-	var dice_scene: Area2D = dice.instantiate()
-	var chosen_path: PathFollow2D = paths.pick_random()
-	chosen_path.add_child(dice_scene)
+@export var dice_count: int = 2  # how many dice to spawn per attack
 
-	$AttackDuration.start()
+func start() -> void:
+	var available_paths: Array = paths.duplicate()
+	available_paths.shuffle()
+
+	for i in range(min(dice_count, available_paths.size())):
+		var chosen_path: PathFollow2D = available_paths[i]
+		chosen_path.progress_ratio = 0.0  # reset in case this path was used before
+		chosen_path.set_process(true)     # re-enable in case it was turned off last time
+
+		var dice_scene: Area2D = dice.instantiate()
+		chosen_path.add_child(dice_scene)
--- code/scripts/boss/attacks/dice_pathing.gd
+++ code/scripts/boss/attacks/dice_pathing.gd
@@ -1,6 +1,16 @@
 extends PathFollow2D
 
 @export var speed: float = randf_range(0.06, 0.1)
+signal end
+
+func _ready() -> void:
+	loop = false  # so progress_ratio clamps at 1.0 instead of wrapping back to 0
 
 func _process(delta: float) -> void:
 	progress_ratio += speed * delta
+
+	if progress_ratio >= 1.0:
+		end.emit()
+		set_process(false)
+		for child in get_children():
+			child.queue_free()
