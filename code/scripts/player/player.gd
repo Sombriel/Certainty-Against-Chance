d79874fb5cc44fbbe75569ee72f9e363ff3c339d
stacks-Gabe <ggarci78@lion.lmu.edu>
2026-06-20 19:36:51 -0700

    made dash more 'cuphead' like

--- code/scripts/player/player.gd
+++ code/scripts/player/player.gd
@@ -11,37 +11,44 @@ const DASH_SPEED: float = 900.0
 
 var is_dashing: bool = false
 var can_dash: bool = true
+var dash_direction: float = 1.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_just_pressed("Dash") and can_dash:
-		is_dashing = true
-		can_dash = false
-		hurtbox.set_deferred("monitoring", false)
-		dash_timer.start()
-		dash_again_timer.start()
-	
-	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		if is_dashing:
-			velocity.x = direction * DASH_SPEED
-		else:
-			velocity.x = direction * SPEED
+	if is_dashing:
+		velocity.x = dash_direction * DASH_SPEED
+		velocity.y = 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
+		var direction: float = Input.get_axis("Left", "Right")
+		
+		if direction != 0:
+			velocity.x = direction * SPEED
+			dash_direction = direction
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		if Input.is_action_just_pressed("Dash") and can_dash:
+			_dash()
 
 	move_and_slide()
+
+func _dash() -> void:
+	is_dashing = true
+	can_dash = false
+	hurtbox.set_deferred("monitoring", false)
+	dash_timer.start()
+	dash_again_timer.start()
 
 func end_of_dash() -> void:
 	hurtbox.set_deferred("monitoring", true)
 	is_dashing = false
+	velocity.x = 0
 
 func _on_dash_again_timer_timeout() -> void:
 	can_dash = true
+	pass
