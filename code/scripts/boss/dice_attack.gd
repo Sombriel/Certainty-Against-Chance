a8aba7a95686eed89e66e7fbbaa7112e1c6bf8b0
stacks-Gabe <ggarci78@lion.lmu.edu>
2026-06-20 23:39:40 -0700

    added three possible paths for dice to follow

--- code/scripts/boss/dice_attack.gd
+++ code/scripts/boss/dice_attack.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 @export var dice: PackedScene
-@onready var spawnpoints: Array = [$SpawnPointLeft, $SpawnPointRight]
+@onready var paths: Array = [$PathONE, $PathTWO, $PathTHREE]
 
 func start() -> void:
-	var dice_scene: Node = dice.instantiate()
+	var dice_scene: Area2D = dice.instantiate()
 	add_child(dice_scene)
-	dice_scene.position = spawnpoints.pick_random().position
-	
+
 	$AttackDuration.start()
 
 func _on_attack_duration_timeout() -> void:
